6689065c3c9805319a9f52a390c7031f411a98c1
Franco Delgado <francodevgado@gmail.com>
2022-06-06 11:40:03 -0300

    added NavBar component (1st component)

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">NavLink</a></li>
+                <li><a href="/">NavLink 2</a></li>
+                <li><a href="/">NavLink 3</a></li>
+            </ul>
+        </nav>
+    )
+}
+
